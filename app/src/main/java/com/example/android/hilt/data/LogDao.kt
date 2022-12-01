98a350c46d2597bea569d88ad164ac7b86a09d2a
unlu <corepavlion@gmail.com>
2022-12-01 23:05:43 +0300

    Test and content provider added

--- app/src/main/java/com/example/android/hilt/data/LogDao.kt
+++ app/src/main/java/com/example/android/hilt/data/LogDao.kt
@@ -1,5 +1,6 @@
 package com.example.android.hilt.data
 
+import android.database.Cursor
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
@@ -15,4 +16,10 @@ interface LogDao {
 
     @Query("DELETE FROM logs")
     fun nukeTable()
+
+    @Query("SELECT * FROM logs ORDER BY id DESC")
+    fun selectAllLogsCursor(): Cursor
+
+    @Query("SELECT * FROM logs WHERE id = :id")
+    fun selectLogById(id: Long): Cursor?
 }
